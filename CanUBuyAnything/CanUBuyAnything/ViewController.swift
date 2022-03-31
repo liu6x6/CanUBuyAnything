275c4cbbeede4779f3fb7d2b5d60a74320e5da6d
Xiao Long Liu <xiao-long.liu@microfocus.com>
2022-03-31 11:38:56 +0800

    init the project

---
+++ CanUBuyAnything/CanUBuyAnything/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CanUBuyAnything
+//
+//  Created by xl-liu on 2022/3/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Can you buy anyring ? No"
+        
+    }
+
+
+}
+
